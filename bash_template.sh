1106a0b24ab72554f59a76f201bfa6dfb8ffd071
Julian Garcia <jg.garcia@globant.com>
2018-08-27 11:36:21 -0500

    First commit Fucntional Bash Template

---
+++ bash_template.sh
@@ -0,0 +1,26 @@
+ alias docu='cd /Users/user_name/Documents'
+ alias down='cd /Users/user_name/Downloads'
+ alias desk='cd /Users/user_name/Desktop'
+ alias repo='cd /Users/user_name/repositories'
+ alias sour='source /Users/user_name/.bash_profile'
+ alias acti='source venv/bin/activate'
+ 
+ alias bashe='vim ~/.bash_profile'
+ alias vimrce='vim ~/.vimrc'
+ 
+ 
+ alias gst='git status'
+ alias gbr='git branch'
+ alias glo='git log'
+ 
+ export LC_ALL=en_US.UTF-8
+ export LANG=en_US.UTF-8
+ 
+ 
+ #color on mac terminal
+ export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+ export CLICOLOR=1
+ export LSCOLORS=ExFxBxDxCxegedabagacad
+ alias ls='ls -GFh'
+ 
+ export PATH=$PATH:$HOME/Library/Python/3.7/bin/
