acf82f03e90e0bfea97f5923ee7337e5237257a4
Julian <julian.programmer.developer@gmail.com.com>
2019-02-23 00:07:51 -0600

    Added: installations bash script

---
+++ installations.sh
@@ -0,0 +1,15 @@
+sudo apt-get -y install git vim-gtk
+# python tools
+sudo apt-get -y install python 3.6.5
+sudo apt-get -y install python-virtualenv
+sudo apt-get -y install python3-pip
+# configurations
+#vimrc
+VIMRC=~/.vimrc
+echo set number > $VIMRC
+echo set hls > $VIMRC
+echo set nowrap > $VIMRC
+echo set autoindent > $VIMRC
+# bash_profile
+touch ~/.bash_profile
+sed 's/user_name/'"$USER"'/g' bash_template.sh > ~/.bash_profile
